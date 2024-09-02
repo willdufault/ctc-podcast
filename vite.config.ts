ca2173a51732ebec9c339d14e98186fea2c5da7d
wduf <wduf02@gmail.com>
2024-09-01 21:26:07 -0400

    updated package json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://willdufault.github.io/ctc-podcast/'
+  base: 'https://willdufault.dev/ctc-podcast/'
 })
