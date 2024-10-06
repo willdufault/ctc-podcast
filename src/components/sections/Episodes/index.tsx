a8c6307916d8546429d9ae5375c00307114d5d0d
wduf <wduf02@gmail.com>
2024-10-06 15:49:36 -0400

    customized carousel + added episodes and guests + removed view all buttons for now

--- src/components/sections/Episodes/index.tsx
+++ src/components/sections/Episodes/index.tsx
@@ -2,28 +2,105 @@ import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
 import Carousel from 'react-multi-carousel'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
+import ctcLogo from '../../../assets/images/coffee-to-campfire-logo.jpg'
 import 'react-multi-carousel/lib/styles.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from '@fortawesome/free-regular-svg-icons'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 export default function Episodes() {
+  interface EpisodeData {
+    title: string
+    description: string
+    date: string
+    hours: number
+    minutes: number
+    url: string
+  }
+  const episodes: EpisodeData[] = [
+    {
+      title: '#4 Jane Doe',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta voluptatibus eos earum aut reiciendis!',
+      date: 'Jan 1 2024',
+      hours: 1,
+      minutes: 35,
+      url: 'https://www.example.com'
+    },
+    {
+      title: '#3 Jane Doe',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta voluptatibus eos earum aut reiciendis!',
+      date: 'Jan 1 2024',
+      hours: 0,
+      minutes: 51,
+      url: 'https://www.example.com'
+    },
+    {
+      title: '#2 Jane Doe',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta voluptatibus eos earum aut reiciendis!',
+      date: 'Jan 1 2024',
+      hours: 1,
+      minutes: 35,
+      url: 'https://www.example.com'
+    },
+    {
+      title: '#1 Jane Doe',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta voluptatibus eos earum aut reiciendis!',
+      date: 'Jan 1 2024',
+      hours: 1,
+      minutes: 35,
+      url: 'https://www.example.com'
+    }
+  ]
+
+  function formatTime(hours: number, minutes: number): string {
+    const hours_formatted = hours > 0 ? `${hours} hr ` : ''
+    const minutes_formatted = `${minutes} min`
+    return hours_formatted + minutes_formatted
+  }
+
+  const ButtonGroup = ({ next, previous, goToSlide, carouselState }: any) => {
+    const { currentSlide } = carouselState;
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <Button onClick={() => previous()} variant='arrow'>
+          <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+        </Button>
+        <ul className='list-inline my-3 px-3'>
+          {episodes.map((_, index) => (
+            <li className='list-inline-item mb-1'>
+              {/* Modulo prevents bug when jumping to slide 0. */}
+              <Button
+                onClick={() => goToSlide(index + episodes.length)}
+                variant='dot'
+                className={currentSlide % episodes.length == index ? "bg-gray-dark" : "initial"}
+              >
+              </Button>
+            </li>
+          ))}
+        </ul>
+        <Button onClick={() => next()} variant='arrow'>
+          <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+        </Button>
+      </div>
+    );
+  };
   return (
     <>
       <Container id='episodes' className='pt-4 pb-5'>
         <h3>Latest Episodes</h3>
         <Carousel
-          autoPlaySpeed={3000}
-          centerMode={false}
-          className=""
-          containerClass="container-with-dots"
-          dotListClass=""
           draggable={false}
-          focusOnSelect={false}
           infinite
-          itemClass=""
-          keyBoardControl
-          minimumTouchDrag={80}
           pauseOnHover
-          renderArrowsWhenDisabled={false}
-          renderButtonGroupOutside={false}
-          renderDotsOutside={false}
+          arrows={false}
+          showDots={false}
+          customButtonGroup={<ButtonGroup />}
+          renderButtonGroupOutside={true}
+          shouldResetAutoplay
+          slidesToSlide={1}
+          swipeable
           responsive={{
             desktop: {
               breakpoint: {
@@ -42,39 +119,35 @@ export default function Episodes() {
               partialVisibilityGutter: 30
             },
           }}
-          rewind={false}
-          rewindWithAnimation={false}
-          rtl={false}
-          shouldResetAutoplay
-          showDots={true}
-          sliderClass=""
-          slidesToSlide={1}
-          swipeable
         >
-          <div className='p-1'>
-            <Card>
-              <Card.Body>test</Card.Body>
-            </Card>
-          </div>
-          <div className='p-1'>
-            <Card>
-              <Card.Body>test</Card.Body>
-            </Card>
-          </div>
-          <div className='p-1'>
-            <Card>
-              <Card.Body>test</Card.Body>
-            </Card>
-          </div>
-          <div className='p-1'>
-            <Card>
-              <Card.Body>test</Card.Body>
-            </Card>
-          </div>
+          {episodes.map((episode) => (
+            <div className='p-1 h-100'>
+              <Card className='h-100'>
+                <Card.Body>
+                  <Row className='align-items-center flex-column flex-xl-row row-gap-3'>
+                    <Col sm={9} md={10} lg={7} xl={5}>
+                      <Image src={ctcLogo} alt="Coffee to Campfire logo" className='w-100' fluid></Image>
+                    </Col>
+                    <Col>
+                      <h6 className='mb-2'>{episode.title}</h6>
+                      <p className='mb-1'>{episode.description}</p>
+                      <p className="text-black-50"><small>{episode.date} – {formatTime(episode.hours, episode.minutes)}</small></p>
+                      <a href={episode.url}>
+                        <Button variant='outline-orange-fill'>
+                          <FontAwesomeIcon icon={faCirclePlay}></FontAwesomeIcon>&nbsp;
+                          Listen now
+                        </Button>
+                      </a>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
         </Carousel>
-        <div className='mt-3 d-flex justify-content-center'>
+        {/* <div className='d-flex justify-content-center'>
           <Button variant='outline-orange-border'>View all episodes &rarr;</Button>
-        </div>
+        </div> */}
       </Container>
     </>
   )
--- src/components/sections/Guests/index.tsx
+++ src/components/sections/Guests/index.tsx
@@ -1,31 +1,97 @@
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCirclePlay } from '@fortawesome/free-regular-svg-icons'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import Container from 'react-bootstrap/Container'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
+import ryanHeadshot from '../../../assets/images/ryan-hu-headshot.jpg'
 
 export default function Guests() {
+  interface GuestData {
+    name: string
+    position: string
+    company: string
+    episodeNumber: number
+    url: string
+  }
+  const guests: GuestData[] = [
+    {
+      name: 'Jane Doe',
+      position: 'Chief Executive Officer',
+      company: 'Nvidia',
+      episodeNumber: 1,
+      url: 'https://example.com',
+    },
+    {
+      name: 'Jane Doe',
+      position: 'Chief Executive Officer',
+      company: 'Nvidia',
+      episodeNumber: 1,
+      url: 'https://example.com',
+    },
+    {
+      name: 'Jane Doe',
+      position: 'Chief Executive Officer',
+      company: 'Nvidia',
+      episodeNumber: 1,
+      url: 'https://example.com',
+    },
+    {
+      name: 'Jane Doe',
+      position: 'Chief Executive Officer',
+      company: 'Nvidia',
+      episodeNumber: 1,
+      url: 'https://example.com',
+    },
+  ]
+
+  const ButtonGroup = ({ next, previous, goToSlide, carouselState }: any) => {
+    const { currentSlide } = carouselState;
+    return (
+      <div className="d-flex justify-content-center align-items-center">
+        <Button onClick={() => previous()} variant='arrow'>
+          <FontAwesomeIcon icon={faChevronLeft}></FontAwesomeIcon>
+        </Button>
+        <ul className='list-inline my-3 px-3'>
+          {guests.map((_, index) => (
+            <li className='list-inline-item mb-1'>
+              {/* Modulo prevents bug when jumping to slide 0. */}
+              <Button
+                onClick={() => goToSlide(index + guests.length)}
+                variant='dot'
+                className={currentSlide % guests.length == index ? "bg-gray-dark" : "initial"}
+              >
+              </Button>
+            </li>
+          ))}
+        </ul>
+        <Button onClick={() => next()} variant='arrow'>
+          <FontAwesomeIcon icon={faChevronRight}></FontAwesomeIcon>
+        </Button>
+      </div>
+    );
+  };
   return (
     <>
       <div id='guests' className='bg-body-tertiary'>
         <Container className='pt-4 pb-5'>
           <h3>Featured Guests</h3>
           <Carousel
-            autoPlaySpeed={3000}
-            centerMode={false}
-            className=""
-            containerClass="container-with-dots"
-            dotListClass=""
             draggable={false}
-            focusOnSelect={false}
             infinite
-            itemClass=""
-            keyBoardControl
-            minimumTouchDrag={80}
             pauseOnHover
-            renderArrowsWhenDisabled={false}
-            renderButtonGroupOutside={false}
-            renderDotsOutside={false}
+            arrows={false}
+            showDots={false}
+            customButtonGroup={<ButtonGroup />}
+            renderButtonGroupOutside={true}
+            shouldResetAutoplay
+            slidesToSlide={1}
+            swipeable
             responsive={{
               desktop: {
                 breakpoint: {
@@ -44,39 +110,35 @@ export default function Guests() {
                 partialVisibilityGutter: 30
               },
             }}
-            rewind={false}
-            rewindWithAnimation={false}
-            rtl={false}
-            shouldResetAutoplay
-            showDots={true}
-            sliderClass=""
-            slidesToSlide={1}
-            swipeable
           >
-            <div className='p-1'>
-              <Card>
-                <Card.Body>test</Card.Body>
-              </Card>
-            </div>
-            <div className='p-1'>
-              <Card>
-                <Card.Body>test</Card.Body>
-              </Card>
-            </div>
-            <div className='p-1'>
-              <Card>
-                <Card.Body>test</Card.Body>
-              </Card>
-            </div>
-            <div className='p-1'>
-              <Card>
-                <Card.Body>test</Card.Body>
-              </Card>
-            </div>
+            {guests.map((guest) => (
+              <div className='p-1 h-100'>
+                <Card className='h-100'>
+                  <Card.Body>
+                    <Row className='align-items-center flex-column flex-lg-row row-gap-3'>
+                      <Col sm={9} md={10} lg={7} xl={6}>
+                        <Image src={ryanHeadshot} alt="Coffee to Campfire logo" className='w-100' fluid></Image>
+                      </Col>
+                      <Col>
+                        <h6 className='mb-2'>{guest.name}</h6>
+                        <p className='mb-1'>{guest.position} at {guest.company}</p>
+                        <p className="text-black-50"><small>Episode #{guest.episodeNumber}</small></p>
+                        <a href={guest.url}>
+                          <Button variant='outline-orange-fill'>
+                            <FontAwesomeIcon icon={faCirclePlay}></FontAwesomeIcon>&nbsp;
+                            Listen now
+                          </Button>
+                        </a>
+                      </Col>
+                    </Row>
+                  </Card.Body>
+                </Card>
+              </div>
+            ))}
           </Carousel>
-          <div className='mt-3 d-flex justify-content-center'>
+          {/* <div className='mt-3 d-flex justify-content-center'>
             <Button variant='outline-orange-border'>View past guests &rarr;</Button>
-          </div>
+          </div> */}
         </Container>
       </div>
     </>
