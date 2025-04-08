603ed640b0a5b3633dac6be3f2138327506e82fe
wduf <wduf02@gmail.com>
2025-04-07 21:29:27 -0400

    added auto year in footer copyright

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,14 @@
 import Container from 'react-bootstrap/Container'
 
 export default function Footer() {
+  const year: number = new Date().getFullYear()
+
   return (
     <>
       <div id="footer" className="bg-dark text-white-50">
         <Container className="py-4">
           <div className="d-flex flex-column flex-md-row align-items-center row-gap-1 justify-content-md-between">
-            <p className="mb-0">Copyright &copy; 2024 Coffee to Campfire</p>
+            <p className="mb-0">Copyright &copy; {year} Coffee to Campfire</p>
             <p className="mb-0">
               Created by{' '}
               <a
